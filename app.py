109178b9133a918f4c4df1765a008ac1a2439af2
Omotola2025 <obembeomotola@gmail.com>
2025-12-12 14:21:30 +0100

    rearrangement

--- app.py
+++ app.py
@@ -120,13 +120,6 @@ This project analyzes daily air quality data collected from major Indian cities 
 The aim of this project is to clean and preprocess the dataset, perform exploratory data analysis (EDA), engineer new features, and build a machine learning model to predict AQI_Bucket. The workflow includes visualizing pollutant behavior, analyzing correlations, training a classification model, and developing a simple GUI application for data exploration and prediction. GitHub is used for version control and project documentation.
 """)
 
-st.subheader("Static Analysis Plots")
-
-
-st.write("""
-The heatmap shows which columns contain missing values and how they are distributed. Most pollutants had scattered missing points, which is common in environmental datasets due to sensor downtime or incomplete reporting. This visualization helped confirm the need for imputation during data cleaning.
-""")
-
 col1, col2, col3 = st.columns(3)
 
 with col1:
@@ -167,6 +160,12 @@ with col3:
     # Divider
     st.markdown("<div class='custom-divider'></div>", unsafe_allow_html=True)
 
+st.subheader("Static Analysis Plots")
+
+
+st.write("""
+The heatmap shows which columns contain missing values and how they are distributed. Most pollutants had scattered missing points, which is common in environmental datasets due to sensor downtime or incomplete reporting. This visualization helped confirm the need for imputation during data cleaning.
+""")
 
 image_path_1 = os.path.join("images", "opk.png")
 
